224cc2420e073c16fccf1bfd7d881610a5799e2d
waleed azhar <waleedazhar@waleeds-mbp.lan>
2017-04-07 20:16:29 -0400

    Initial Commit

---
+++ Income & Sales Tax/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Income & Sales Tax
+//
+//  Created by waleed azhar on 2017-04-07.
+//  Copyright © 2017 waleed azhar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
